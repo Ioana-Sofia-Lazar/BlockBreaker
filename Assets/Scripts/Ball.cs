d5ae4c4f3ad3295cfb5c6b38d6d9bf5f2fdf79b4
Ioana Lazar <ioana.pascu9596@yahoo.com>
2020-04-13 19:13:35 +0300

    Launch ball on mouse click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,11 @@ public class Ball : MonoBehaviour
 {
 
     [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     Vector2 paddleToBallDistance;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,25 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePosition + paddleToBallDistance;
