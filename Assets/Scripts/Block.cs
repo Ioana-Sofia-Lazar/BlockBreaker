6c69e9272c5e84e56dd55146e98fe947d6b2eea5
Ioana Lazar <ioana.pascu9596@yahoo.com>
2020-04-14 20:38:56 +0300

    Calculate total number of breakable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.IncrementBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
