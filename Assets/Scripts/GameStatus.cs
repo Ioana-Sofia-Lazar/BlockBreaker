06e47aaad2f89e6c938b8aba1e7221cf28ee252c
Ioana Lazar <ioana.pascu9596@yahoo.com>
2020-04-15 20:31:13 +0300

    Display score on screen

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
 
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlock = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlock;
+        scoreText.text = currentScore.ToString();
     }
 }
