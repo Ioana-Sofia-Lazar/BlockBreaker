0720651b575bb2ad4f66bf76df3394df0a8230ac
Ioana Lazar <ioana.pascu9596@yahoo.com>
2020-04-17 20:13:42 +0300

    Add auto play option (test purposes)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -9,18 +9,33 @@ public class Paddle : MonoBehaviour
     [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
 
+    GameSession gameSession;
+    Ball ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameSession = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
-        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        paddlePosition.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePosition;
+    }
+
+    private float GetXPos()
+    {
+        if (gameSession.IsAutoPlayEnabled())
+        {
+            return ball.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
